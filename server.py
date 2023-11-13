d93e97434942bc41405d0711aa6a5d851d1d31dd
hammmu <104701130+hammmu@users.noreply.github.com>
2023-11-13 12:55:12 +0500

    Update server.py

--- server.py
+++ server.py
@@ -21,7 +21,7 @@ async def send_image(websocket, path):
     except websockets.ConnectionClosed:
         print("Connection closed by the client")
 
-start_server = websockets.serve(send_image, "172.174.202.227", 80)
+start_server = websockets.serve(send_image, "*", 80)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
